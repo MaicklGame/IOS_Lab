c72d364f095571a5e994e095692bbf79256b781e
Mikhail Pavlov <maicklgame@gmail.com>
2024-02-23 23:19:02 +0600

    main

---
+++ EffectiveSwiftLab13v/Models/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  EffectiveSwiftLab13v
+//
+//  Created by Mikhail Pavlov.
+//
+
+import Foundation
+
+struct Category {
+    var imageName: String
+    var name: String
+}
---
+++ EffectiveSwiftLab13v/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  EffectiveSwiftLab13v
+//
+//  Created by Mikhail Pavlov.
+//
+
+import Foundation
+struct User {
+    var id: UUID = UUID()
+    var imageAvatarName: String
+    var name: String
+}
